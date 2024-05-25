356583e4fb754874cbb0c98aaaf1daf3177757da
joykgg <joru446@gmail.com>
2024-05-25 11:22:36 +0200

    Update UploadImage.jsx ls
    lss

--- src/components/uploadImage/UploadImage.jsx
+++ src/components/uploadImage/UploadImage.jsx
@@ -26,7 +26,7 @@ const UploadImage = () => {
 
     try {
       const response = await axios.post(
-        "https://e339-35-233-190-208.ngrok-free.app/predict",
+        "https://6707-34-27-19-206.ngrok-free.app/predict",
         formData,
         {
           headers: {
