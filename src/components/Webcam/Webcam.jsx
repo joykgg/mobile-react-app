2ace917be25d281a8724d669389fe63a4bf29d68
joykgg <joru446@gmail.com>
2024-06-03 12:59:41 +0200

    Update Webcam.jsx lkk
    lkk

--- src/components/Webcam/Webcam.jsx
+++ src/components/Webcam/Webcam.jsx
@@ -34,7 +34,7 @@ export const WebcamCapture = () => {
       formData.append("image", blob, "captured-image.jpg");
 
       const response = await axios.post(
-        "https://2914-34-74-89-189.ngrok-free.app/predict",
+        "https://6c79-34-75-50-92.ngrok-free.app/predict",
         formData,
         {
           headers: {
